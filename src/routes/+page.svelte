93942c00de722025c36effd4f62e713c368e91e6
Amir <amirbennasr@gmail.com>
2023-07-07 14:11:27 +0100

    first push

---
+++ src/routes/+page.svelte
@@ -0,0 +1,141 @@
+<script>
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import bg from '$lib/images/bg-school.jpg'
+</script>
+
+<!-- <svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head> -->
+
+<div class="hero">
+	<div class="right-banner">
+
+		<div class="content">
+			<span class="content__subtitle">ACADEMICS</span>
+			<span class="content__title">OPENING UP A WORLD OF EDUCATION</span>
+			<span class="content__description">We believe that there is nothing more important than skillful education. Doing the right thing. at the right time</span>
+			<a href="#">Know More</a>
+		</div>
+
+	</div>
+	<img src={bg} alt="" srcset="">
+
+</div>
+
+
+
+<style>
+
+	 
+	 @media (width < 1000px ) 
+    {
+        span {
+			font-size: 0.8em !important;
+		}
+		.content__title {
+		font-weight: bold;
+		font-size: 3.5em !important;
+		line-height: 4.2rem;
+	}
+	.content__description {
+		font-weight: 400;
+		font-size: 0.8em !important;
+	}
+    }
+	
+	@media (width < 750px)
+	{
+		.right-banner {
+		position: absolute !important;
+		height: 50% !important;
+		width: 80% !important;
+		top: 25% !important;
+		right: 12.5% !important;
+		}
+		.content {
+		
+			align-items: center !important;
+			justify-content: center !important;
+			gap: 0.5rem !important;
+		}
+		.content__title {
+		font-weight: bold;
+		font-size: 1.5em !important;
+		line-height: 2.2rem !important;
+	}
+	.content__description {
+		font-weight: 400;
+		line-height: 1.4em !important;
+		font-size: 0.8em !important;
+	}
+		.hero {
+			height: calc(100vh - 75px) !important;
+		}
+	}
+	a {
+		text-decoration: none;
+		color: white;
+		font-weight: bold;
+		border: 2px solid white;
+		padding-inline: 1.5rem;
+		padding-block: 0.5rem;
+		width: fit-content;
+
+	}
+	span {
+		color: white;
+		font-size: 1em;
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+		max-width: auto;
+		margin: 12rem;
+		gap: 1.5rem;
+	}
+	.content__subtitle {
+		font-weight: 200;
+		letter-spacing: 0.6rem;
+	}
+	.content__title {
+		font-weight: bold;
+		font-size: 4.5em;
+		line-height: 4.2rem;
+	}
+	.content__description {
+		font-weight: 400;
+		font-size: 1.2em;
+	}
+	.right-banner {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		
+	}
+
+	.hero {
+		height: 100vh;
+		width: 100%;
+		position: relative;
+	}
+
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: fill;
+	}
+
+	.right-banner {
+		background: blue;
+		opacity: 0.7;
+		position: absolute;
+		z-index: 55;
+		width: 50%;
+		height: 100%;
+		right: 0;
+		top: 0;
+	}
+
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,133 @@
+<script>
+	import svgLogo from '$lib/images/github.svg';
+    import gsap from 'gsap';
+	import { onMount } from 'svelte';
+
+    let animate ;
+    let menuOpen = false;
+onMount(()=> {
+    animate =  gsap.fromTo('.menu',{
+        translateX:'-100%',
+        duration:'0.4',
+        ease:'easeIn',
+    },
+    {
+        translateX:'160',
+        duration:'0.4',
+        ease:'easeIn',
+        paused:true
+
+    },
+    )
+
+ 
+    
+})
+	function toggleMenu()  {
+        menuOpen = !menuOpen;
+        if(menuOpen) {
+            animate.play();
+        }
+        else {
+            animate.reverse();
+        }
+    }
+</script>
+
+<nav>
+		<img src={svgLogo} alt="" srcset="" class="fa-xl" />
+    <a href="http://" on:click={()=>toggleMenu()}>
+        <i class="fa-solid fa-bars fa-xl"></i>
+    </a>
+        <i class="fa-solid fa-magnifying-glass fa-xl"></i>
+
+  
+    
+</nav>
+<div class="menu">
+	<div class="menu__sidebar">
+		<ul class="menu__list">
+			<li>
+                <a href="/">Home</a>
+            </li>
+			<li><a href="/about">About</a></li>
+			<li><a href="contact">Contact</a></li>
+		</ul>
+	</div>
+	<div class="menu__content" >
+        <h1>Hello world</h1>
+        <p>Some content about the selected page</p>
+    </div>
+</div>
+
+<style>
+
+    @media (width < 750px)
+    {
+        nav {
+            position: relative !important;
+            height: 75px !important;
+        }   
+    }
+   
+    ul {
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+    }
+    li {
+        list-style: none;
+        font-weight: 700;
+        font-size: 3rem;
+    }
+	.menu {
+        height: 100vh;
+		background: white;
+        position: fixed;
+        top: 0;
+        left: 0;
+        transform: translateX(-100%);
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: stretch;
+        justify-content: center;
+        z-index: 69;
+        
+	}
+
+   
+
+    .menu__sidebar {
+        flex: 1;
+        place-self: center;
+    }
+
+    .menu__content {
+        
+        flex: 1;
+        place-self: center;
+    }
+
+	nav {
+		background: white;
+		position: fixed;
+		width: 160px;
+		height: 100%;
+		left: 0;
+		top: 0;
+        z-index: 999;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center; 
+	}
+	.logo {
+		max-width: 1rem;
+	}
+	img {
+        width: 3rem;
+		max-width: 100%;
+		max-height: 100%;
+	}
+</style>
